1986ebbb21b5f43214c6795f78215f36ab92739c
Augtons <79037928+Augtons@users.noreply.github.com>
2023-11-27 21:08:22 +0800

    1. 实现 cio istream ostream 接口的定义

--- Src/Include/cio.h
+++ Src/Include/cio.h
@@ -20,9 +20,37 @@
 #define CIO_MAX(a, b) ((a) > (b) ? (a) : (b))
 #define CIO_MIN(a, b) ((a) < (b) ? (a) : (b))
 
+#define CIO_CONTAINER_OF(pointer, type, member) ((type*)(((char*)(pointer)) - (char*)(&(((type*)0)->member))))
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef enum cio_err {
+    CIO_OK
+} cio_err_t;
+
+typedef struct cio_istream cio_istream_t;
+typedef struct cio_ostream cio_ostream_t;
+
+struct cio_istream {
+    cio_err_t (*cio_open)(struct cio_istream *self);
+    cio_err_t (*cio_close)(struct cio_istream *self);
+    size_t (*cio_read_func)(struct cio_istream *self, uint8_t *buf, size_t len, size_t timeout);
+};
+
+struct cio_ostream {
+    cio_err_t (*cio_open)(struct cio_ostream *self);
+    cio_err_t (*cio_close)(struct cio_ostream *self);
+    size_t (*cio_write_func)(struct cio_ostream *self, const uint8_t *buf, size_t len, size_t timeout);
+};
+
+cio_err_t cio_istream_open(struct cio_istream *self);
+cio_err_t cio_istream_close(struct cio_istream *self);
+cio_err_t cio_ostream_open(struct cio_ostream *self);
+cio_err_t cio_ostream_close(struct cio_ostream *self);
+size_t cio_read(cio_istream_t *self, uint8_t *buf, size_t len, size_t timeout);
+size_t cio_write(cio_ostream_t *self, const uint8_t *buf, size_t len, size_t timeout);
 
 #ifdef __cplusplus
 }
--- Src/cio.c
+++ Src/cio.c
@@ -1,1 +1,31 @@
 #include "cio.h"
+
+cio_err_t cio_istream_open(struct cio_istream *self)
+{
+    return self->cio_open(self);
+}
+
+cio_err_t cio_istream_close(struct cio_istream *self)
+{
+    return self->cio_close(self);
+}
+
+cio_err_t cio_ostream_open(struct cio_ostream *self)
+{
+    return self->cio_open(self);
+}
+
+cio_err_t cio_ostream_close(struct cio_ostream *self)
+{
+    return self->cio_close(self);
+}
+
+size_t cio_read(cio_istream_t *self, uint8_t *buf, size_t len, size_t timeout)
+{
+    return self->cio_read_func(self, buf, len, timeout);
+}
+
+size_t cio_write(cio_ostream_t *self, const uint8_t *buf, size_t len, size_t timeout)
+{
+    return self->cio_write_func(self, buf, len, timeout);
+}
